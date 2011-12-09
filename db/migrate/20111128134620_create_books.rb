0ece07da67313706be980fa367169862701d86bc
Kei Inagawa <k.inaga1028@gmail.com>
2011-12-09 21:43:12 +0900

    first commit

---
+++ db/migrate/20111128134620_create_books.rb
@@ -0,0 +1,17 @@
+class CreateBooks < ActiveRecord::Migration
+  def self.up
+    create_table :books do |t|
+      t.string :isbn
+      t.integer :price
+      t.string :publish
+      t.date :published
+      t.boolean :cd
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
